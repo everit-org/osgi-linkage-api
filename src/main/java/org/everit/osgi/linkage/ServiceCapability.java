eaec317ac13f1b4d76a554064321891b18006ed8
Balazs Zsoldos <balazs.zsoldos@everit.biz>
2015-02-19 10:25:21 +0100

    Adding ServiceCapability and ConfigurableResource

---
+++ src/main/java/org/everit/osgi/linkage/ServiceCapability.java
@@ -0,0 +1,50 @@
+/**
+ * This file is part of Everit - OSGi Linkage API.
+ *
+ * Everit - OSGi Linkage API is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Everit - OSGi Linkage API is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Everit - OSGi Linkage API.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.everit.osgi.linkage;
+
+import org.osgi.framework.ServiceReference;
+import org.osgi.resource.Capability;
+
+/**
+ * A {@link Capability} that represents and OSGi service.
+ */
+public interface ServiceCapability extends Capability {
+
+    /**
+     * Namespace that is returned if the {@link Capability} is a {@link ServiceCapability}.
+     */
+    String SERVICE_CAPABILITY_NAMESPACE = "osgi.service";
+
+    /**
+     * Two {@link ServiceCapability}s are the same if they hold the same {@link ServiceReference} instance.
+     */
+    @Override
+    boolean equals(Object obj);
+
+    /**
+     * Returns the {@link ServiceReference} that is held by this {@link Capability}.
+     *
+     * @return The {@link ServiceReference} that is held by this {@link Capability}.
+     */
+    ServiceReference<?> getServiceReference();
+
+    /**
+     * Two {@link ServiceCapability}s have the same hashcode if they hold the same {@link ServiceReference} instance.
+     */
+    @Override
+    int hashCode();
+}
